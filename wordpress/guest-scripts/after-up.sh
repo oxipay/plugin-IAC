6964c6723c0941aa2c0fd9d157b63fe2f99e75eb
Troy Wright <Troy.Wright@certegy.com.au>
2016-09-30 08:14:44 +0930

    Corrected line ending problems
    # Conflicts:
    #	wordpress/guest-scripts/after-up.sh

--- wordpress/guest-scripts/after-up.sh
+++ wordpress/guest-scripts/after-up.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
-sudo -i
+#!/bin/bash
+sudo -i;
 if service --status-all | grep -Fq 'docker'; then
-	echo 'vagrant: starting docker daemon'
-	cd /src/wordpress-environment
-	docker-compose up -d
-	service docker restart
+	echo 'vagrant: starting docker daemon';
+	cd /src/wordpress-environment;
+	docker-compose up -d;
+	service docker restart;
 fi
