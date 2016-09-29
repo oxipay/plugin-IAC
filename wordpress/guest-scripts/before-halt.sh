3e85e4f94a25a343bbf10b799a9c4f9fe5145916
Troy Wright <Troy.Wright@certegy.com.au>
2016-09-30 08:24:13 +0930

    Another line ending fix
    (cherry picked from commit 6e05b7662def33586223fb0abd1546740a7e7665)
    
    # Conflicts:
    #	wordpress/guest-scripts/before-halt.sh

--- wordpress/guest-scripts/before-halt.sh
+++ wordpress/guest-scripts/before-halt.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
-sudo -i
+#!/bin/bash
+sudo -i;
 if service --status-all | grep -Fq 'docker'; then
-	cd /src/wordpress-environment
-	docker-compose down
-	service docker stop
+	cd /src/wordpress-environment;
+	docker-compose down;
+	service docker stop;
 fi
