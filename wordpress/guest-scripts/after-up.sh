96791bdcb471f61e19945f601d40bbef0d11cf55
BradTrahar <Brad.Trahar@certegy.com.au>
2016-10-04 13:35:39 +1030

    Added proxy stuff to WordPress wp-config.php file

--- wordpress/guest-scripts/after-up.sh
+++ wordpress/guest-scripts/after-up.sh
@@ -4,5 +4,6 @@ if service --status-all | grep -Fq 'docker'; then
 	echo 'vagrant: starting docker daemon';
 	cd /src/wordpress-environment;
 	docker-compose up -d;
+	/src/scripts/set_wordpress_proxy.sh
 	service docker restart;
 fi
---
+++ wordpress/guest-scripts/set_wordpress_proxy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+#Set the file to use (Must exist)
+file="/src/wordpress/wp-config.php"
+#
+#Check if file exists if not loop da loop
+i=0
+while [ ! -e $file ]; do
+        i=$((i+1))
+        sleep 15
+        if [ $i -eq 8 ]; then
+                exit
+        fi
+done
+#
+#Check if the proxy server is set
+if [ -u $1 ]; then
+        exit
+else
+        server="$1"
+fi
+#
+#Check if the proxy port is set
+if [ -u $2 ]; then
+        exit
+else
+        port="$2"
+fi
+#
+#If the file does not exist then exit
+if [ ! -e $file ]; then
+        exit
+fi
+#
+#If the file is a regular file then write into it
+if [ -f $file ]; then
+       echo "" >> "$file";
+       echo "/* Configure proxy Server */" >> "$file";
+       echo "define('WP_PROXY_HOST', '$server');" >> "$file";
+       echo "define('WP_PROXY_PORT', '$port');" >> "$file";
+       echo "define('WP_PROXY_USERNAME', '');" >> "$file";
+       echo "define('WP_PROXY_PASSWORD', '');" >> "$file";
+       echo "define('WP_PROXY_BYPASS_HOSTS', 'localhost, 127.0.0.1');" >> "$file";
+fi
+#
+#Job Done
